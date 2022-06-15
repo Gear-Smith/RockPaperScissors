9372fff15ffa335102686a6b8393283def76be9f
BJ Jackson <smthbrothrjdev@gmail.com>
2022-06-15 12:09:54 -0400

    add index.html boilerplat, css, and eventlistener

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("on load")
+button= document.querySelectorAll(".buttons")
+button.forEach(element => {
+    element.addEventListener('click', playerChoice)
+});
+
+function playerChoice(event){
+console.log(event)
+console.log(event.currentTarget.id)
+}
