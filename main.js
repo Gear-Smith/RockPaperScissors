a967993778effae547d714e28834b0c2198d28e0
BJ Jackson <smthbrothrjdev@gmail.com>
2022-06-15 20:45:19 -0400

    add business logic functions

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ button.forEach(element => {
 });
 
 function playerChoice(event){
-console.log(event)
-console.log(event.currentTarget.id)
+ console.log(event.currentTarget.id)
+ decide(event.currentTarget.id)
+}
+
+function computerChoice(){
+    // 1 = rock | 2 = paper | 3 = Scissors
+    const rNum = Math.floor(Math.random()*3+1)
+    
+    return rNum
+}
+
+function decide(player){
+    
+    const computer= computerChoice()
+    const dict = ['rock','paper','scissors']
+    
+    console.log(dict[computer-1], player)
+    if (player === dict[computer-1]){
+        console.log('its a tie')
+    
+    }
+    else if (player == 1 )
+    }
 }
